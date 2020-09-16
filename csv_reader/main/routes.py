d4e42d02acd8071a5af5741015a5e1a34f611b8b
arshaan <arshaan256@gmail.com>
2020-09-17 04:07:08 +0530

    Added page bar

--- csv_reader/main/routes.py
+++ csv_reader/main/routes.py
@@ -33,9 +33,53 @@ def home():
         return redirect(url_for('main.home'))
     return render_template("home.html", title="Home Page", form=form)
 
-@main.route("/display/<int:page>", methods=["GET"])
-def displayData(page):
-    rows_per_page = 100
-    data = Data.query.order_by(Data.col1.asc()).paginate(page=page, per_page=rows_per_page, error_out=False)
-    data_ = Data.query.all()
-    return render_template("display_data.html", title="Display Data", data=data)
+@main.route("/display", methods=["GET"])
+def displayData():
+    context = dict()
+    context["page_num"] = request.args.get('page_num', 1, type=int)
+    context["per_page"] = request.args.get('per_page', 100, type=int) 
+    context["order_by"] = request.args.get('order_by', None)
+    context["sort_by"] = request.args.get('sort_by', None)
+
+    if context["order_by"] == "id":
+        if context["sort_by"] == "asc":
+            data = Data.query.order_by(Data.id.asc()).paginate(page=context["page_num"], 
+                                                            per_page=context["per_page"], 
+                                                            error_out=False)
+        else:
+            data = Data.query.order_by(Data.id.desc()).paginate(page=context["page_num"], 
+                                                            per_page=context["per_page"], 
+                                                            error_out=False)
+    elif context["order_by"] == "col1":
+        if context["sort_by"] == "asc":
+            data = Data.query.order_by(Data.col1.asc()).paginate(page=context["page_num"], 
+                                                            per_page=context["per_page"], 
+                                                            error_out=False)
+        else:
+            data = Data.query.order_by(Data.col1.desc()).paginate(page=context["page_num"], 
+                                                            per_page=context["per_page"], 
+                                                            error_out=False)
+    elif context["order_by"] == "col2":
+        if context["sort_by"] == "asc":
+            data = Data.query.order_by(Data.col2.asc()).paginate(page=context["page_num"], 
+                                                            per_page=context["per_page"], 
+                                                            error_out=False)
+        else:
+            data = Data.query.order_by(Data.col2.desc()).paginate(page=context["page_num"], 
+                                                            per_page=context["per_page"], 
+                                                            error_out=False)  
+    elif context["order_by"] == "col3":
+        if context["sort_by"] == "asc":
+            data = Data.query.order_by(Data.col3.asc()).paginate(page=context["page_num"], 
+                                                            per_page=context["per_page"], 
+                                                            error_out=False)
+        else:
+            data = Data.query.order_by(Data.col3.desc()).paginate(page=context["page_num"], 
+                                                            per_page=context["per_page"], 
+                                                            error_out=False)   
+    else:
+        data = Data.query.order_by(Data.id.asc()).paginate(page=context["page_num"], 
+                                                            per_page=context["per_page"], 
+                                                            error_out=False)
+         
+    return render_template("display_data.html", title="Display Data", data=data, context=context)
